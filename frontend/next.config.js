4567c2343479baf832ab276f903e7333a2b10449
mittalsneha760@gmail.com <mittalsneha760@gmail.com>
2026-02-21 17:30:45 +0530

    Updated project

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
+  output: "export",
   env: {
     NEXT_PUBLIC_API_URL:
       process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api",
